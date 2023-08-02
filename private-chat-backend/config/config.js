af46756982947e1272a6de48ab02e517970d03b5
Caio Lima <caiojlimah@gmail.com>
2023-08-02 06:34:42 -0300

    [FEAT]: Migrations and Models creation with Sequelize

--- private-chat-backend/config/config.js
+++ private-chat-backend/config/config.js
@@ -2,26 +2,26 @@ require('dotenv').config();
 
 const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST } = process.env;
 
-module.export = {
-  "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: "mysql"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: "mysql"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: "mysql"
   }
 };
